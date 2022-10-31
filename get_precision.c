34f53b83a4ccf82a8345b38dc66dc61faa804fce
Abelj <111173212+abeljemal22@users.noreply.github.com>
2022-10-31 17:33:05 +0300

    driver.c

--- get_precision.c
+++ get_precision.c
@@ -1,41 +1,36 @@
 #include "main.h"
 
 /**
- * get_precision - Calculates the precision for printing
- * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed.
- * @list: list of arguments.
- *
- * Return: Precision.
+ * driver - selector for type of fun.
+ * @format: string.
+ * Description: the function loops through the structs
+ * selector[] ment of the struct.
+ * Return: a pointer
+ * structype selector - Struct
  */
-int get_precision(const char *format, int *i, va_list list)
+
+int (*driver(char *format))(char *format, va_list)
 {
-	int curr_i = *i + 1;
-	int precision = -1;
+int i;
 
-	if (format[curr_i] != '.')
-		return (precision);
+structype selector[] = {
+{"%c", printc},
+{"%s", printstr},
+{"%d", printint},
+{"%i", printint},
+{"%%", printpercent},
+{"%x", printhex},
+{"%X", printHEX},
+{"%o", printocta},
+{NULL, NULL}
+};
 
-	precision = 0;
-
-	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
-	{
-		if (is_digit(format[curr_i]))
-		{
-			precision *= 10;
-			precision += format[curr_i] - '0';
-		}
-		else if (format[curr_i] == '*')
-		{
-			curr_i++;
-			precision = va_arg(list, int);
-			break;
-		}
-		else
-			break;
-	}
-
-	*i = curr_i - 1;
-
-	return (precision);
+if (format[1] == ' ' || format[1] == '\0')
+return (NULL);
+for (i = 0; selector[i].q; i++)
+{
+if (format[1] == selector[i].q[1])
+return (selector[i].u);
+}
+return (NULL);
 }
