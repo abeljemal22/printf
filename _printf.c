c742d373ee923ec67b9d3bae602ad3c76860cb46
Abelj <111173212+abeljemal22@users.noreply.github.com>
2022-11-05 17:40:22 +0300

    _printf.c

--- _printf.c
+++ _printf.c
@@ -3,35 +3,52 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
-#define MAX_BUF 1024
 /**
- * _printf - prints the input on the shell buffer
- * @format: requested format
+ * _printf - formatted output conversion and print data.
+ * @format: input string.
  *
- * Return: a length of the string outputed
+ * Return: number of chars printed.
  */
 int _printf(const char *format, ...)
 {
-	int value, i;
-	va_list args;
-	char *result;
+	unsigned int i = 0, len = 0, ibuf = 0;
+	va_list arguments;
+	int (*function)(va_list, char *, unsigned int);
+	char *buffer;
 
-	if (!format || args == NULL)
+	va_start(arguments, format), buffer = malloc(sizeof(char) * 1024);
+	if (!format || !buffer || (format[i] == '%' && !format[i + 1]))
 		return (-1);
-	if (format[0] == '%' && !format[1])
-		return (-1);
-	result = malloc(MAX_BUF);
-	if (!result)
-		return (-1);
-	for (i = 0; i < MAX_BUF; i++)
-		result[i] = '\0';
-	va_start(args, format);
-	value = get_formater(result, format, args);
-	while (*result)
+	if (!format[i])
+		return (0);
+	for (i = 0; format && format[i]; i++)
 	{
-		_write(*result);
-		result++;
+		if (format[i] == '%')
+		{
+			if (format[i + 1] == '\0')
+			{	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
+				return (-1);
+			}
+			else
+			{	function = get_print_func(format, i + 1);
+				if (function == NULL)
+				{
+					if (format[i + 1] == ' ' && !format[i + 2])
+						return (-1);
+					handl_buf(buffer, format[i], ibuf), len++, i--;
+				}
+				else
+				{
+					len += function(arguments, buffer, ibuf);
+					i += ev_print_func(format, i + 1);
+				}
+			} i++;
+		}
+		else
+			handl_buf(buffer, format[i], ibuf), len++;
+		for (ibuf = len; ibuf > 1024; ibuf -= 1024)
+			;
 	}
-	va_end(args);
-	return (value);
+	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
+	return (len);
 }
