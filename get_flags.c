5bc8fa5060d244dfd25f6a6f27da1da49c31d0be
Abelj <111173212+abeljemal22@users.noreply.github.com>
2022-11-05 18:06:33 +0300

    ev_print_func.c

--- get_flags.c
+++ get_flags.c
@@ -1,20 +1,51 @@
 #include "main.h"
 
 /**
- * _puts - prints a string
- * new line, to stdout.
- * @string: input string
- * Return: count of string.
+ * ev_print_func - returns the amount of identifiers.
+ * @s: argument indentifier
+ * @index: index of argument identifier.
+ * Return: amount of identifiers.
  */
-int _puts(char *string)
+int ev_print_func(const char *s, int index)
 {
-	int contador = 0;
+	print_t pr[] = {
+		{"c", print_chr}, {"s", print_str}, {"i", print_int},
+		{"d", print_int}, {"b", print_bnr}, {"u", print_unt},
+		{"o", print_oct}, {"x", print_hex}, {"X", print_upx},
+		{"S", print_usr}, {"p", print_add}, {"li", prinlint},
+		{"ld", prinlint}, {"lu", prinlunt}, {"lo", prinloct},
+		{"lx", prinlhex}, {"lX", prinlupx}, {"hi", prinhint},
+		{"hd", prinhint}, {"hu", prinhunt}, {"ho", prinhoct},
+		{"hx", prinhhex}, {"hX", prinhupx}, {"#o", prinnoct},
+		{"#x", prinnhex}, {"#X", prinnupx}, {"#i", print_int},
+		{"#d", print_int}, {"#u", print_unt}, {"+i", prinpint},
+		{"+d", prinpint}, {"+u", print_unt}, {"+o", print_oct},
+		{"+x", print_hex}, {"+X", print_upx}, {" i", prinsint},
+		{" d", prinsint}, {" u", print_unt}, {" o", print_oct},
+		{" x", print_hex}, {" X", print_upx}, {"R", print_rot},
+		{"r", print_rev}, {"%", print_prg}, {"l", print_prg},
+		{"h", print_prg}, {" +i", prinpint}, {" +d", prinpint},
+		{"+ i", prinpint}, {"+ d", prinpint}, {" %", print_prg},
+		{NULL, NULL},
+	};
+	int i = 0, j = 0, first_index;
 
-	while (*string)
+	first_index = index;
+	while (pr[i].type_arg)
 	{
-		_putchar(*string);
-		string++;
-		contador++;
+		if (s[index] == pr[i].type_arg[j])
+		{
+			if (pr[i].type_arg[j + 1] != '\0')
+				index++, j++;
+			else
+				break;
+		}
+		else
+		{
+			j = 0;
+			i++;
+			index = first_index;
+		}
 	}
-	return (contador);
+	return (j);
 }
